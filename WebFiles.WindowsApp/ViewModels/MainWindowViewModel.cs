fa05d0452ebb32186d05444a40c501989a7eb254
李嘉俊 <2383690354@qq.com>
2023-07-16 22:23:30 +0800

    asdf

---
+++ WebFiles.WindowsApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WebFiles.WindowsApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ WebFiles.WindowsApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace WebFiles.WindowsApp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
